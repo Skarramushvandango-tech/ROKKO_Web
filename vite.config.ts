502ae60af4111f1ae83bc6cbb029f162f2f5b15a
Skarramushvandango-tech <Skarramush.vandango@gmx.de>
2025-09-29 13:27:33 +0200

    vite.config.ts aktualisieren

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ROKKO_Web/',   // <- wichtig für GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
