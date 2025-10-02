3724eeb57e779aafc4c5e43209b9914da64ecd2a
Skarramushvandango-tech <Skarramush.vandango@gmx.de>
2025-10-02 17:14:38 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
+const root = document.getElementById("root");
+if (!root) {
+  throw new Error("Root element #root not found in index.html");
+}
+createRoot(root).render(<App />);
