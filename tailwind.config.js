4e665ce4dc428b17b726ea6428986eb2b508ea11
Skarramushvandango-tech <Skarramush.vandango@gmx.de>
2025-10-02 20:34:01 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        bg: "#262626",
+        accent1: "#483D03",
+        text: "#F5F3BB",
+        accent2: "#96897B",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
