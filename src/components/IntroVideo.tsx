0d9c86e166ed25158c3a02e256ad26870864709d
Skarramushvandango-tech <Skarramush.vandango@gmx.de>
2025-10-02 22:55:38 +0200

    Reduce padding in IntroVideo component

--- src/components/IntroVideo.tsx
+++ src/components/IntroVideo.tsx
@@ -3,7 +3,7 @@ import { asset } from "../utils/asset";
 
 export default function IntroVideo() {
   return (
-    <section className="mx-auto max-w-5xl px-4 py-8">
+    <section className="mx-auto max-w-5xl px-4 py-6">
       <div className="aspect-video w-full overflow-hidden rounded-md border border-white/10 bg-black/20">
         <video
           src={asset("/movies/intro_movie.mp4")}
