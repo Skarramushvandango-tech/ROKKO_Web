c956d35f7ae6b0c8abceb301275848f49dfb6c28
Skarramushvandango-tech <Skarramush.vandango@gmx.de>
2025-10-02 14:56:02 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const container = document.getElementById("root")!;
-createRoot(container).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(container).render(<App />);
