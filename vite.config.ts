49686efcad6ea0a18e047a06e9c56bb51b1d6ecd
Skarramushvandango-tech <Skarramush.vandango@gmx.de>
2025-10-02 18:45:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// /vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ROKKO_Web/",
   plugins: [react()],
-  build: { outDir: "dist" }
+  base: "/ROKKO_Web/"
 });
