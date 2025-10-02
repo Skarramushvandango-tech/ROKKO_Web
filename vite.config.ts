2783945b09465c19ea1a2b526bddc742edd03891
Skarramushvandango-tech <Skarramush.vandango@gmx.de>
2025-10-02 13:39:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// GitHub Pages unter /ROKKO_Web/
 export default defineConfig({
-  base: '/',                 // wichtig für Netlify
+  base: '/ROKKO_Web/',
   plugins: [react()],
   optimizeDeps: { exclude: ['lucide-react'] },
 });
